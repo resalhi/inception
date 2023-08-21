0b2a4b118eb51ab6b666885cbe06e6668f74fa50
resalhi <ressalhi.70@gmail.com>
2023-08-21 08:27:09 -1000

    almost done

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -1,6 +1,10 @@
 #!bin/sh
-if [ ! -f "/var/www/wp-config.php" ]; then
-    cat << EOF > /var/www/wp-config.php
+
+wget https://wordpress.org/latest.zip && unzip latest.zip && cp -rf wordpress/* . && rm -rf wordpress latest.zip
+
+# mv wp-config-sample.php wp-config.php
+
+cat << EOF > wp-config.php
     <?php
     define( 'DB_NAME', '${DB_NAME}' );
     define( 'DB_USER', '${DB_USER}' );
@@ -15,4 +19,11 @@ if [ ! -f "/var/www/wp-config.php" ]; then
     define( 'ABSPATH', __DIR__ . '/' );}
     require_once ABSPATH . 'wp-settings.php';
 EOF
-fi
+
+# sed -i -r "s/database_name_here/$DB_NAME/1" wp-config.php
+# sed -i -r "s/username_here/$DB_USER/1"       wp-config.php
+# sed -i -r "s/password_here/$DB_PASS/1"        wp-config.php
+
+sed -i -r "s/localhost/mariadb/1"    wp-config.php 
+
+/usr/sbin/php-fpm8 -F -R
