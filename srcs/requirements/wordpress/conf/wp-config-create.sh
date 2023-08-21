8aa1633db99caf7a7d427ff7686c26e974f6972d
resalhi <ressalhi.70@gmail.com>
2023-08-21 11:28:09 -1000

    needs volumes

--- srcs/requirements/wordpress/conf/wp-config-create.sh
+++ srcs/requirements/wordpress/conf/wp-config-create.sh
@@ -2,28 +2,23 @@
 
 wget https://wordpress.org/latest.zip && unzip latest.zip && cp -rf wordpress/* . && rm -rf wordpress latest.zip
 
-# mv wp-config-sample.php wp-config.php
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 
-cat << EOF > wp-config.php
-    <?php
-    define( 'DB_NAME', '${DB_NAME}' );
-    define( 'DB_USER', '${DB_USER}' );
-    define( 'DB_PASSWORD', '${DB_PASS}' );
-    define( 'DB_HOST', 'mariadb' );
-    define( 'DB_CHARSET', 'utf8' );
-    define( 'DB_COLLATE', '' );
-    define('FS_METHOD','direct');
-    \$table_prefix = 'wp_';
-    define( 'WP_DEBUG', false );
-    if ( ! defined( 'ABSPATH' ) ) {
-    define( 'ABSPATH', __DIR__ . '/' );}
-    require_once ABSPATH . 'wp-settings.php';
-EOF
+chmod +x wp-cli.phar
 
-# sed -i -r "s/database_name_here/$DB_NAME/1" wp-config.php
-# sed -i -r "s/username_here/$DB_USER/1"       wp-config.php
-# sed -i -r "s/password_here/$DB_PASS/1"        wp-config.php
+mv wp-cli.phar /usr/local/bin/wp
 
+wp core download --force --allow-root  --version=5.8.1 --locale=en_US
+
+mv wp-config-sample.php wp-config.php
+
+sed -i -r "s/database_name_here/$DB_NAME/1" wp-config.php
+sed -i -r "s/username_here/$DB_USER/1"       wp-config.php
+sed -i -r "s/password_here/$DB_PASS/1"        wp-config.php
 sed -i -r "s/localhost/mariadb/1"    wp-config.php 
+
+wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_TITLE --admin_password=$DB_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+
+wp user create $DB_USER $WP_ADMIN_EMAIL --role=author --user_pass=$DB_PASS --allow-root
 
 /usr/sbin/php-fpm8 -F -R
