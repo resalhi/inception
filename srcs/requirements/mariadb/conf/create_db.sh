4644a871d6c5ff304efcd1049fe1bcbbf8d8f1a6
resalhi <ridaessalhi.70@gmail.com>
2023-08-27 12:03:58 -0400

    done..

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "CREATE DATABASE $DB_NAME ;" > database.sql
-echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';" >> database.sql
+echo "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$WP_USER'@'%' IDENTIFIED BY '$DB_PASS';" >> database.sql
 echo "FLUSH PRIVILEGES;" >> database.sql
 
 mysqld --user=mysql --init-file=/database.sql
